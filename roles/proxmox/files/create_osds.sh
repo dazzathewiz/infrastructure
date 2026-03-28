35fa600052ad0e250813679814ed814ba8e32d7b
dazzathewiz <dazzathewiz@gmail.com>
2026-03-29 09:07:37 +1000

    (update) permanent removal of prox3 & prox4 hardware configs from cluster as part of inf rationalisation. A new host with same name will be re-joined in future.

--- roles/proxmox/files/create_osds.sh
+++ roles/proxmox/files/create_osds.sh
@@ -12,11 +12,6 @@ pveceph osd create --encrypted 1 /dev/sda
 pveceph osd create --encrypted 1 /dev/sdc
 ceph-volume lvm batch --osds-per-device 4 --crush-device-class nvme --dmcrypt /dev/nvme1n1
 
-# Prox3
-pveceph osd create --encrypted 1 /dev/sde
-pveceph osd create --encrypted 1 /dev/sdf
-ceph-volume lvm batch --osds-per-device 4 --crush-device-class nvme --dmcrypt /dev/nvme1n1
-
 # Prox 5
 ceph-volume lvm batch --osds-per-device 2 --crush-device-class nvme --dmcrypt /dev/nvme1n1
 
